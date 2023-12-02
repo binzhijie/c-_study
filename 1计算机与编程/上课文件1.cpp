15aad3e9a8a746fa696ba8928ef19d20d9740e7a
binzhijie <bzjvip@126.com>
2023-12-02 10:48:07 +0800

    新增

--- 1计算机与编程/上课文件1.cpp
+++ 1计算机与编程/上课文件1.cpp
@@ -1,6 +1,16 @@
 #include<iostream> 
 using namespace std;
 int main() {
-	cout << "Hello,World!";
+	cout << "    *" << endl;
+	cout << "   ***" << endl;
+	cout << "  *****" << endl;
+	cout << "    *" << endl;
+	cout << "   ***" << endl;
+	cout << "  *****" << endl;
+	cout << " *******" << endl;
+	cout << "   | |" << endl;
+	cout << "   |_|" <<endl;
+	cout << endl << "BinZhijie's" << endl;
+	
 	return 0;
 }
---
+++ 2算数运算符/main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout << 8000/3600 <<"时"<<8000%3600/60<<"分"<<8000%60<<"秒";
+	return 0;
+}
